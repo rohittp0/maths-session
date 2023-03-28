91e19afd595408d3f9204a05f935c4776ddb93be
Rohit T P <tprohit9@gmail.com>
2023-03-28 18:15:38 +0530

    highlight required points
    Signed-off-by: Rohit T P <tprohit9@gmail.com>

--- fractal/fit.py
+++ fractal/fit.py
@@ -23,19 +23,20 @@ def get_coefficients(n,d, x, y):
 
 
 def main():
-    x = [0, 1/3, 1/2, 3/4, 1]
-    y = [1/5, 1/2, 1/3, 3/4, 1/2]
+    x = [0, 1 / 3, 1 / 2, 3 / 4, 1]
+    y = [1 / 5, 1 / 2, 1 / 3, 3 / 4, 1 / 2]
 
-    coefficients = [get_coefficients(n, scales[n-1](x[n]), x, y) for n in range(1, len(x))]
+    coefficients = [get_coefficients(n, scales[n - 1](x[n]), x, y) for n in range(1, len(x))]
 
     point = (0, 0)
     # plt.axis([0, 100, 0, 100])
+    plt.plot(x, y, "ro", markersize=2)
 
-    for _ in range(10000):
-        k = random.randint(0, len(x)-2)
+    for _ in range(1000):
+        n = random.randint(0, len(x) - 2)
 
-        a, e, c, f = coefficients[k]
-        point = (a*point[0] + e, c*point[0] + scales[k](point[0])*point[1] + f)
+        a, e, c, f = coefficients[n]
+        point = (a * point[0] + e, c * point[0] + scales[n](point[0]) * point[1] + f)
 
         plt.plot(*point, '.', markersize=1)
 
